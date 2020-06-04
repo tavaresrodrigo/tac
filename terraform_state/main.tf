7c57150ecab34baf445626cb2a45fd443f6bb6b1
rodrigo.actavares@gmail.com <rodrigo.actavares@gmail.com>
2020-06-04 21:52:40 +0100

    Add TF state

---
+++ terraform_state/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-tavana"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
