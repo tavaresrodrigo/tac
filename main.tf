ce6c3a7caa5b657327ce63ab144f7d7f1657f75a
rodrigo.actavares@gmail.com <rodrigo.actavares@gmail.com>
2020-11-17 15:54:56 +0000

    ASG config

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 // Provider
-
 provider "aws" {
   region = "us-east-2"
 }
 
 // Data sources
-
 data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
@@ -21,6 +19,11 @@ variable "server_port" {
   default = 8080
 }
 
+variable "lb_port" {
+  description = "Application Load Balancer Port"
+  type = number
+  default = 80
+}
 
 // Resources
 resource "aws_launch_configuration" "example" {
@@ -52,6 +55,8 @@ resource "aws_security_group" "instance" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier = data.aws_subnet_ids.default.ids
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -63,8 +68,82 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = var.lb_port
+  protocol = "HTTP"
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+}
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
 // Outputs
-//output "public_ip" {
-//  value = aws_instance.example.public_ip
-//  description = "Instance public Ip"
-// }
+output "alb_dns_namme" {
+  value = aws_lb.example.dns_name
+  description = "ALB DNS name, public endpoint"
+ }
